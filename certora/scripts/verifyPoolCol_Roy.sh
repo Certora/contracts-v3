2a48e7faebbf68f2eb8723bcebf295c9379dd136
Roy-Certora <101589076+Roy-Certora@users.noreply.github.com>
2022-05-09 10:33:07 +0300

    Updated Pool Collection specs
    Added some getters for Pool Collection harness. Added invariants to the spec and a script for windows. A new dummy Poo Token for BNT was created at helpers.

---
+++ certora/scripts/verifyPoolCol_Roy.sh
@@ -0,0 +1,35 @@
+py ../../EVMVerifier/scripts/certoraRun.py \
+    certora/helpers/Receiver1.sol \
+    certora/harness/PoolCollectionHarness.sol \
+    certora/helpers/DummyERC20A.sol certora/helpers/DummyERC20B.sol \
+    certora/munged/pools/BNTPool.sol certora/munged/network/NetworkSettings.sol \
+    certora/munged/network/BancorNetwork.sol \
+    certora/munged/pools/PoolTokenFactory.sol certora/munged/pools/PoolToken.sol \
+    certora/helpers/DummyERC20bnt.sol \
+    certora/helpers/DummyPoolTokenA.sol certora/helpers/DummyPoolTokenB.sol \
+    certora/helpers/DummyPoolTokenBNT.sol certora/munged/vaults/MasterVault.sol \
+    certora/helpers/DummyTokenGovernanceA.sol certora/helpers/DummyTokenGovernanceB.sol \
+    certora/munged/vaults/ExternalProtectionVault.sol \
+    --verify PoolCollectionHarness:certora/spec/PoolCol.spec \
+    --link  PoolCollectionHarness:_bnt=DummyERC20bnt \
+            PoolCollectionHarness:_masterVault=MasterVault \
+            PoolCollectionHarness:_bntPool=BNTPool \
+            PoolCollectionHarness:_externalProtectionVault=ExternalProtectionVault \
+            BNTPool:_masterVault=MasterVault \
+            BNTPool:_poolToken=PoolToken \
+            BNTPool:_bnt=DummyERC20bnt \
+            BNTPool:_bntGovernance=DummyTokenGovernanceA \
+            BNTPool:_vbntGovernance=DummyTokenGovernanceB \
+            MasterVault:_bntGovernance=DummyTokenGovernanceA \
+            MasterVault:_vbntGovernance=DummyTokenGovernanceB \
+    --solc solc8.13 \
+    --staging \
+    --settings -divideNoRemainder=true \
+    --send_only \
+    --rule consistentTradingLiquidity \
+    --method "tradeBySourceAmount(bytes32,address,address,uint256,uint256)" \
+    --optimistic_loop \
+    --packages_path node_modules \
+    --packages @openzeppelin=node_modules/@openzeppelin \
+    @bancor=node_modules/@bancor \
+    --msg "PoolCol - consistentTradingLiquidity"
